f92f536087b01d7e858529883c7823e1cf03a623
Kyle Tucker <kyleatucker@gmail.com>
2016-04-11 14:14:09 -0400

    add session post type

---
+++ src/PostType/Session.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ASMBS\ScheduleBuilder\PostType;
+
+
+/**
+ * This post type represents any session in the conference program -- pre-conference courses, plenary sessions,
+ * social events, etc., are all considered _sessions_.
+ *
+ * @author  Kyle Tucker <kyleatucker@gmail.com>
+ */
+class Session extends AbstractPostType
+{
+    const SLUG = 'session';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSingularLabel()
+    {
+        return 'Session';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPluralLabel()
+    {
+        return 'Sessions';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getArgs()
+    {
+        return [
+            'labels' => [
+                'menu_name' => 'Schedule',
+            ],
+            'menu_icon' => 'dashicons-calendar-alt',
+        ];
+    }
+}
