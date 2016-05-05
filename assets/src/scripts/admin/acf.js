925672457119596467b713d09e843a9ecd7bcf34
Kyle Tucker <kyleatucker@gmail.com>
2016-05-05 14:41:34 -0400

    begin js refactor

--- assets/src/scripts/admin/acf.js
+++ assets/src/scripts/admin/acf.js
@@ -1,92 +1,58 @@
 !function($, w, d) {
 
-    var _acf = $.ScheduleBuilder.ACF = {};
+    var sb = w.sb = {};
 
-    _acf.VenueManager = {
-
-        select2: false,
-        $venue: null,
-        $room: null,
-        defaultChoices: null,
-
-        init: function() {
-            var venueSelector = '#acf-location--venue',
-                roomSelector = '#acf-location--room';
-
-            if ($(venueSelector).data('ui') == 1) {
-                // For Select2 support
-                this.select2 = true;
-                venueSelector = venueSelector +'-input';
-            }
-
-            this.$venue = $(venueSelector);
-            this.$room = $(roomSelector);
-
-            this.defaultChoices = this.$room.children();
-
-            this.$venue.on('change init', this.loadRooms).trigger('init');
+    acf.inputmask = acf.model.extend({
+        
+        events: {
+            'domchange.sb ': '_load',
         },
-
-        loadRooms: function() {
-            var self = _acf.VenueManager;
-
-            $.ajax(w.ajaxurl, {
-                data: {
-                    action: 'sb/load_rooms',
-                    nonce: w.sb_acf.nonce,
-                    venue: self.$venue.val(),
-                    post: w.sb_acf.post,
-                },
-                dataType: 'json',
-            }).done(function(data) {
-                self.$room.empty();
-                self.$room.append(self.defaultChoices);
-                $.each(data, function(i, room) {
-                    self.addRoom(room);
-                });
-                if (self.select2) {
-                    acf.select2.init(self.$room);
-                }
-            });
-        },
-
-        addRoom: function(room) {
-            var $option = this.$room.children('[value="'+ room.name +'"]');
-            if ($option.length == 0) {
-                $option = $(d.createElement('option'))
-                    .attr('value', room.name)
-                    .text(room.name);
-                if (room.selected) {
-                    $option.attr('selected', 'selected');
-                }
-            }
-
-            this.$room.append($option);
-        },
-    };
-
-    _acf.InputMask = {
 
         masks: [
             {
                 selector: '.acf-time input',
-                mask: 'h:s',
+                mask:     'h:s',
+                options:  {},
             }
         ],
 
         init: function() {
-            $.each(this.masks, function(i, mask) {
-                $(mask.selector).inputmask($.extend({
-                    mask: mask.mask
-                }, mask.options));
+            this._load();
+        },
+
+        _load: function() {
+            $.each(this.masks, function(i, maskConfig) {
+                $(maskConfig.selector).inputmask($.extend({
+                    mask: maskConfig.mask
+                }, maskConfig.options));
             });
         }
-    };
+    });
+
+    var domchanger = acf.model.extend({
+        _domchange: function() {
+            $(d).trigger('domchange.sb');
+        }
+    })
+
+    acf.fields.repeater = acf.fields.repeater
+        .extend(domchanger)
+        .extend({
+            events: {
+                'click a[data-event="add-row"]': '_domchange',
+            }
+        });
+
+    acf.fields.flexible_content = acf.fields.flexible_content
+        .extend(domchanger)
+        .extend({
+            events: {
+                'click .acf-fc-popup a': '_domchange',
+            }
+        });
 
     $(d).ready(function() {
-        // acf.unload.active = 0;
-        _acf.VenueManager.init();
-        _acf.InputMask.init();
+        acf.inputmask.init();
     });
 
 }(jQuery, window, document);
