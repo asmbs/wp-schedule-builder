366d27e92806a02f7e1840240fef7211645a4c95
Kyle Tucker <kyleatucker@gmail.com>
2016-05-06 12:06:51 -0400

    finish js refactor

--- assets/src/scripts/admin/acf.js
+++ assets/src/scripts/admin/acf.js
@@ -2,57 +2,140 @@
 
     var sb = w.sb = {};
 
-    acf.inputmask = acf.model.extend({
-        
-        events: {
-            'domchange.sb ': '_load',
-        },
+    /**
+     * Venue manager -- dynamically loads room choices based on the
+     * selected venue.
+     */
+    sb.venueManager = {
 
-        masks: [
-            {
-                selector: '.acf-time input',
-                mask:     'h:s',
-                options:  {},
-            }
-        ],
+        $venueField: null,
+        $roomField: null,
 
+        select2: false,
+        defaultChoices: null,
+
+        /**
+         * Initialize the field relationship.
+         */
         init: function() {
-            this._load();
+            // Set field references
+            this.$venueField = $('#acf-location--venue');
+            this.$roomField = $('#acf-location--room');
+
+            if (this.$venueField.data('ui') == 1) {
+                // If the Select2 UI is enabled, swap the venue field reference to the *-input sibling
+                this.select2 = true;
+                this.$venueField = $(this.$venueField.selector +'-input');
+            }
+
+            // Get the default room choice list
+            this.defaultChoices = this.$roomField.children();
+
+            // Bind handlers
+            this.$venueField
+                .on('change.venuemanager init.venuemanager', this.load_rooms)
+                .trigger('init.venuemanager');
         },
 
-        _load: function() {
-            $.each(this.masks, function(i, maskConfig) {
-                $(maskConfig.selector).inputmask($.extend({
-                    mask: maskConfig.mask
-                }, maskConfig.options));
+        /**
+         * Issue an XHR request to load the room choices for a selected venue.
+         */
+        load_rooms: function(e) {
+            // Get self-reference
+            var that = sb.venueManager,
+                ajaxurl = w.ajaxurl,
+                opts = w.sb_acf;
+
+            // Run XHR request
+            $.ajax(ajaxurl, {
+                dataType: 'json',
+                data: {
+                    action: 'sb/load_rooms',
+                    nonce:  opts.nonce,
+                    post:   opts.post,
+                    venue:  that.$venueField.val(),
+                },
+            }).done(that._set_rooms);
+        },
+
+        /**
+         * Populate the room list using _add_room.
+         */
+        _set_rooms: function(data) {
+            // Get self-reference
+            var that = sb.venueManager;
+
+            // Reset the room list with the default choices
+            that.$roomField.empty()
+                .append(that.defaultChoices);
+
+            // Add each room in the returned list
+            $.each(data, function(i, room) {
+                that._add_room(room);
             });
+
+            // Reinitialize select2
+            if (that.select2) {
+                acf.select2.init(that.$roomField);
+            }
+        },
+
+        /**
+         * Add a room to the room list.
+         */
+        _add_room: function(room) {
+            var $o = this.$roomField.children('[value="'+ room.name +'"]');
+            if ($o.length == 0) {
+                $o = $(d.createElement('option'))
+                    .attr('value', room.name)
+                    .text(room.name);
+                if (room.selected) {
+                    $o.attr('selected', 'selected');
+                }
+            }
+
+            this.$roomField.append($o);
+        },
+    };
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Inputmask extension -- helper for adding input masks to arbitrary
+     * acf fields.
+     */
+    acf.inputmask = acf.model.extend({
+
+        mask: function(e, mask, args) {
+            e.$el.inputmask(mask, args);
+        },
+
+        unmask: function(e) {
+            e.$el.inputmask('remove');
         }
     });
 
-    var domchanger = acf.model.extend({
-        _domchange: function() {
-            $(d).trigger('domchange.sb');
-        }
-    })
+    /**
+     * Time field -- extends the basic text field to add a time-formatted
+     * input mask.
+     */
+    acf.fields.time = acf.field.extend({
 
-    acf.fields.repeater = acf.fields.repeater
-        .extend(domchanger)
-        .extend({
-            events: {
-                'click a[data-event="add-row"]': '_domchange',
-            }
-        });
+        type: 'time',
 
-    acf.fields.flexible_content = acf.fields.flexible_content
-        .extend(domchanger)
-        .extend({
-            events: {
-                'click .acf-fc-popup a': '_domchange',
-            }
-        });
+        events: {
+            'focus input': 'mask_time',
+        },
+
+        mask_time: function(e) {
+            acf.inputmask.mask(e, 'h:s');
+        },
+    });
+
+    // -----------------------------------------------------------------------------------------------------------------
 
     $(d).ready(function() {
-        acf.inputmask.init();
+        sb.venueManager.init();
     });
 
 }(jQuery, window, document);
