3105636782bf6843d9a786ab379b944ee0c92853
Kyle Tucker <kyleatucker@gmail.com>
2016-05-11 12:30:53 -0400

    build out post model api and add session model

---
+++ src/Model/AbstractModel.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace ASMBS\ScheduleBuilder\Model;
+
+/**
+ * @author  Kyle Tucker <kyleatucker@gmail.com>
+ */
+abstract class AbstractModel implements ModelInterface
+{
+    /** @var  \WP_Post */
+    protected $post;
+
+    /** @var  \WP_Error */
+    protected $errors;
+
+    /** @var  int */
+    protected $postID;
+
+    /** @var  \WP_User */
+    protected $author;
+
+    /** @var  \DateTime */
+    protected $dateModified;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($post = null)
+    {
+        $this->errors = new \WP_Error();
+        $post = get_post($post);
+
+        if (!($post instanceof \WP_Post)) {
+            $this->errors->add('INVALID', 'Expected a `WP_Post` or integer.', gettype($post));
+
+            return;
+        } elseif (!in_array($post->post_type, $this->getSupportedPostTypes())) {
+            $this->errors->add('INVALID', 'Post type is not supported.', $post->post_type);
+
+            return;
+        }
+
+        $this->post = $post;
+        $this->postID = $post->ID;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasErrors()
+    {
+        return ($this->errors instanceof \WP_Error && count($this->errors->get_error_messages()) > 0);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * Merge errors from another WP_Error object into the one dedicated to this model object.
+     *
+     * @param  \WP_Error  $otherErrors
+     */
+    protected function importErrors(\WP_Error $otherErrors)
+    {
+        foreach ($otherErrors->get_error_codes() as $code) {
+            $errors = $otherErrors->get_error_messages($code);
+            $data = $otherErrors->get_error_data($code);
+            for ($i = 0; $i < max(count($errors), count($data)); $i++) {
+                if (array_key_exists($i, $errors)) {
+                    $data = array_key_exists($i, $data) ? $data[$i] : null;
+                    $this->errors->add($code, $errors[$i], $data);
+                }
+            }
+        }
+    }
+    
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * @return  int
+     */
+    public function getPostID()
+    {
+        return $this->lazyLoad('postID', [$this, 'loadPostProperty'], 'ID');
+    }
+
+    /**
+     * @return  \WP_User
+     */
+    public function getAuthor()
+    {
+        return $this->lazyLoad('author', 'get_user_by', 'ID', $this->post->post_author);
+    }
+
+    /**
+     * @param   string|bool  $format
+     * @return  string|\DateTime
+     */
+    public function getDateModified($format = 'm/d/y')
+    {
+        $datetime = $this->lazyLoad('dateModified', function(\WP_Post $post) {
+            try {
+                return new \DateTime($post->post_modified);
+            } catch (\Exception $e) {
+                return false;
+            }
+        }, $this->post);
+
+        if ($datetime instanceof \DateTime) {
+            return ($format === false) ? $datetime : $datetime->format($format);
+        }
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Lazy-load a class property.
+     *
+     * @param   string    $prop
+     * @param   callable  $callback
+     * @param   mixed     $arg
+     * @return  mixed
+     */
+    protected function lazyLoad($prop, callable $callback, $arg = null)
+    {
+        if ($this->{$prop} !== null) {
+            return $this->{$prop};
+        }
+
+        $args = func_get_args();
+        array_shift($args);
+        array_shift($args);
+
+        $this->{$prop} = call_user_func_array($callback, $args);
+
+        return $this->{$prop};
+    }
+
+    /**
+     * Get a property from the WP_Post instance.
+     *
+     * @param   string  $property
+     * @return  mixed
+     */
+    protected function loadPostProperty($property)
+    {
+        if (isset($this->post->{$property})) {
+            return $this->post->{$property};
+        }
+    }
+
+    /**
+     * Load the terms from the given taxonomy associated with the underlying post.
+     *
+     * @param   string  $taxonomy
+     * @return  \WP_Term[]
+     */
+    protected function loadPostTerms($taxonomy)
+    {
+        $terms = get_the_terms($this->post, $taxonomy);
+        if ($terms instanceof \WP_Error) {
+            $this->importErrors($terms);
+        }
+
+        if (is_array($terms)) {
+            return $terms;
+        }
+
+        return [];
+    }
+
+    /**
+     * Load a _single_ term associated with the post.
+     *
+     * @param   string  $taxonomy
+     * @param   int     $index
+     * @return  \WP_Term
+     */
+    protected function loadSingleTerm($taxonomy, $index = 0)
+    {
+        $terms = $this->loadPostTerms($taxonomy);
+
+        if ($index > 0 && !array_key_exists($index, $terms)) {
+            $index = 0;
+        }
+
+        return $terms[$index];
+    }
+
+    /**
+     * Load a simple custom field.
+     *
+     * @param   string  $fieldName
+     * @return  mixed
+     */
+    protected function loadField($fieldName)
+    {
+        return get_field($fieldName, $this->postID);
+    }
+
+    /**
+     * Load a date field (and optionally a time field) as a DateTime object.
+     *
+     * @param   string  $dateField
+     * @param   string  $timeField
+     * @return  \DateTime|bool
+     */
+    protected function loadFieldAsDateTime($dateField, $timeField = null)
+    {
+        $str = get_field($dateField, $this->postID);
+        if ($timeField) {
+            $str .= ' '. get_field($timeField, $this->postID);
+        }
+
+        try {
+            return new \DateTime($str);
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+}
---
+++ src/Model/Session.php
@@ -0,0 +1,267 @@
+<?php
+
+namespace ASMBS\ScheduleBuilder\Model;
+
+use ASMBS\ScheduleBuilder\PostType;
+use ASMBS\ScheduleBuilder\Taxonomy\SessionTag;
+use ASMBS\ScheduleBuilder\Taxonomy\SessionType;
+use ASMBS\ScheduleBuilder\Taxonomy\Society;
+
+/**
+ * @author  Kyle Tucker <kyleatucker@gmail.com>
+ */
+class Session extends AbstractModel
+{
+    /** @var  string */
+    protected $sessionID;
+    
+    /** @var  string */
+    protected $title;
+
+    /** @var  string */
+    protected $content;
+
+    /** @var  \DateTime */
+    protected $startDate;
+    
+    /** @var  \DateTime */
+    protected $endDate;
+
+    /** @var  string */
+    protected $venue;
+    
+    /** @var  string */
+    protected $room;
+
+    /** @var  string */
+    protected $credits;
+    
+    /** @var  string[] */
+    protected $creditTypes;
+
+    /** @var  null */
+    protected $facultyGroups;
+    
+    /** @var  null */
+    protected $agendaItems;
+
+    /** @var  \WP_Term[] */
+    protected $societies;
+
+    /** @var  \WP_Term */
+    protected $sessionType;
+
+    /** @var  \WP_Term[] */
+    protected $tags;
+
+    /** @var  string[] */
+    protected $progress;
+
+    /**
+     * @return  array
+     */
+    public function getSupportedPostTypes()
+    {
+        return [
+            PostType\Session::SLUG,
+        ];
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * @return  string
+     */
+    public function getSessionID()
+    {
+        return $this->lazyLoad('sessionID', [$this, 'loadField'], 'session_id');
+    }
+
+    /**
+     * @param   bool  $filtered
+     * @return  string
+     */
+    public function getTitle($filtered = true)
+    {
+        $title = $this->lazyLoad('title', [$this, 'loadPostProperty'], 'post_title');
+
+        if ($filtered) {
+            $title = apply_filters('the_title', $title);
+
+            /**
+             * Filter the session title.
+             *
+             * @param   string  $title
+             * @return  string
+             */
+            $title = apply_filters('session_title', $title);
+
+            return $title;
+        }
+
+        return $title;
+    }
+
+    /**
+     * @param   bool  $filtered
+     * @return  string
+     */
+    public function getContent($filtered = true)
+    {
+        $content = $this->lazyLoad('content', [$this, 'loadPostProperty'], 'post_content');
+
+        if ($filtered) {
+            $content = apply_filters('the_content', $content);
+
+            /**
+             * Filter the session content body _after_ `the_content` filters have been applied.
+             *
+             * @param   string  $content
+             * @return  string
+             */
+            $content = apply_filters('session_content', $content);
+
+            return $content;
+        }
+
+        return $content;
+    }
+
+    /**
+     * @param   string|bool  $format
+     * @return  string|\DateTime
+     */
+    public function getDate($format = 'l (n/j)')
+    {
+        $datetime = $this->lazyLoad('startDate', [$this, 'loadFieldAsDateTime'], 'date', 'start_time');
+        if ($datetime instanceof \DateTime) {
+            return ($format === false) ? $datetime : $datetime->format($format);
+        }
+    }
+
+    /**
+     * @param   string|bool  $format
+     * @return  string|\DateTime
+     */
+    public function getStartTime($format = 'g:ia')
+    {
+        return $this->getDate($format);
+    }
+
+    /**
+     * @param   string|bool  $format
+     * @return  string|\DateTime
+     */
+    public function getEndTime($format = 'g:ia')
+    {
+        $datetime = $this->lazyLoad('endDate', [$this, 'loadFieldAsDateTime'], 'date', 'end_time');
+        if ($datetime instanceof \DateTime) {
+            return ($format === false) ? $datetime : $datetime->format($format);
+        }
+    }
+
+    /**
+     * @return  string
+     */
+    public function getVenue()
+    {
+        return $this->lazyLoad('venue', [$this, 'loadField'], 'venue');
+    }
+
+    /**
+     * @return  string
+     */
+    public function getRoom()
+    {
+        return $this->lazyLoad('room', [$this, 'loadField'], 'room');
+    }
+
+    /**
+     * @return  string
+     */
+    public function getCredits()
+    {
+        $credits = $this->lazyLoad('credits', [$this, 'loadField'], 'credits_available');
+        $credits = $credits ? $credits : '0';
+
+        return number_format($credits, 2);
+    }
+
+    /**
+     * @return  string[]
+     */
+    public function getCreditTypes()
+    {
+        if ($this->getCredits() > 0) {
+            return (array) $this->lazyLoad('creditTypes', [$this, 'loadField'], 'credit_types');
+        }
+
+        return [];
+    }
+
+    /**
+     * TODO
+     */
+    public function getFacultyGroups()
+    {}
+
+    /**
+     * TODO
+     */
+    public function getAgendaItems()
+    {}
+
+    /**
+     * @return  \WP_Term[]
+     */
+    public function getSocieties()
+    {
+        return $this->lazyLoad('societies', [$this, 'loadPostTerms'], Society::SLUG);
+    }
+
+    /**
+     * @return  \WP_Term
+     */
+    public function getSessionType()
+    {
+        return $this->lazyLoad('sessionType', [$this, 'loadSingleTerm'], SessionType::SLUG);
+    }
+
+    /**
+     * @return  \WP_Term[]
+     */
+    public function getTags()
+    {
+        return $this->lazyLoad('tags', [$this, 'loadPostTerms'], SessionTag::SLUG);
+    }
+
+    /**
+     * @return  string[]
+     */
+    public function getProgress()
+    {
+        return (array) $this->lazyLoad('progress', [$this, 'loadField'], 'progress');
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Determine whether there are credits available for the session.
+     *
+     * @return  bool
+     */
+    public function hasCredits()
+    {
+        return ($this->getCredits() > 0 && !empty($this->getCreditTypes()));
+    }
+
+    /**
+     * Determine whether the session is editorially complete.
+     *
+     * @return  bool
+     */
+    public function isComplete()
+    {
+        return (count($this->getProgress()) === 5);
+    }
+}
